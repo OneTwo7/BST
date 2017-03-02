350bf480279dc933fa6ff15db13a3228aad1fefd
onetwo7 <lahoohal@gmail.com>
2017-03-02 18:02:29 +0300

    Some progress

--- src/BST.java
+++ src/BST.java
@@ -3,23 +3,29 @@ import java.util.Stack;
 interface Tree {
 
     // опрос размера дерева
+    int getLength ();
 
     // очистка дерева
+    void clearTree ();
 
     // проверка дерева на пустоту
+    boolean isEmpty ();
 
     // поиск элемента с заданным ключом
+    Node findNode (int key);
 
     // включение нового элемента с заданным ключом
     void addNode (int key);
 
     // удаление элемента с заданным ключом
+    void removeNode (int key);
 
     // итератор для доступа к элементам дерева
 
     // обход дерева по схеме: нерекурсивной (Lt -> t -> Rt)
 
     // определение длины внутреннего пути дерева
+    int getInternalPathLength ();
 
     // вывод структуры дерева на экран
     void showTree ();
@@ -35,7 +41,7 @@ class Node {
     Node left;
     Node right;
 
-    public Node (int key) {
+    Node (int key) {
         this.key = key;
         left = null;
         right = null;
@@ -57,13 +63,55 @@ class Iterator {
 
 public class BST implements Tree {
 
-    public static Node root;
+    private static Node root;
+    private int length;
 
-    public BST () {
+    private BST () {
         root = null;
     }
 
+    public int getLength () {
+        return length;
+    }
+
+    public void clearTree () {
+        root = null;
+        length = 0;
+    }
+
+    public boolean isEmpty () {
+        return root == null;
+    }
+
+    public Node findNode(int key) {
+        if (root == null) {
+            System.out.println("The tree is empty.");
+            return null;
+        }
+        Node current = root;
+        while (true) {
+            if (key == current.key) {
+                return current;
+            } else if (key < current.key) {
+                if (current.left == null) {
+                    System.out.println("There's no such element in the tree.");
+                    return null;
+                } else {
+                    current = current.left;
+                }
+            } else {
+                if (current.right == null) {
+                    System.out.println("There's no such element in the tree.");
+                    return null;
+                } else {
+                    current = current.right;
+                }
+            }
+        }
+    }
+
     public void addNode (int key) {
+        length++;
         Node newOne = new Node(key);
         if (root == null) {
             root = newOne;
@@ -73,7 +121,10 @@ public class BST implements Tree {
         Node parent = null;
         while (true) {
             parent = current;
-            if (key < current.key) {
+            if (key == current.key) {
+                length--;
+                return;
+            } else if (key < current.key) {
                 current = current.left;
                 if (current == null) {
                     parent.left = newOne;
@@ -85,6 +136,106 @@ public class BST implements Tree {
                     parent.right = newOne;
                     return;
                 }
+            }
+        }
+    }
+
+    public void removeNode(int key) {
+        if (root == null) {
+            System.out.println("The tree is empty.");
+            return;
+        }
+        length--;
+        Node current = root;
+        Node parent = root;
+        boolean isLeft = false;
+        while (key != current.key) {
+            parent = current;
+            if (key < current.key) {
+                isLeft = true;
+                current = current.left;
+            } else {
+                isLeft = false;
+                current = current.right;
+            }
+            if (current == null) {
+                length++;
+                System.out.println("There's no such element in the tree.");
+                return;
+            }
+        }
+        if (current.left == null && current.right == null) {
+            if (current == root) {
+                root = null;
+            }
+            if (isLeft) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        } else if (current.right == null) {
+            if (current == root) {
+                root = current.left;
+            }
+            if (isLeft) {
+                parent.left = current.left;
+            } else {
+                parent.right = current.left;
+            }
+        } else if (current.left == null) {
+            if (current == root) {
+                root = current.right;
+            }
+            if (isLeft) {
+                parent.left = current.right;
+            } else {
+                parent.right = current.right;
+            }
+        } else {
+            Node replacement = null;
+            Node repParent = null;
+            Node repCurrent = current.right;
+            while (repCurrent != null) {
+                repParent = replacement;
+                replacement = repCurrent;
+                repCurrent = repCurrent.left;
+            }
+            if (replacement != current.right) {
+                repParent.left = replacement.right;
+                replacement.right = current.right;
+            }
+            if (current == root) {
+                root = replacement;
+            }
+            if (isLeft) {
+                parent.left = replacement;
+            } else {
+                parent.right = replacement;
+            }
+            replacement.left = current.left;
+        }
+    }
+
+    public int getInternalPathLength() {
+
+        /*
+            МЕСТО ПОСЛЕДНЕГО ВЫДОХА ГОСПОДИНА ПЖ
+            НУЖНО БОЛЬШЕ СТАКОВ!!!
+        */
+
+        if (root == null) {
+            System.out.println("The tree is empty.");
+            return 0;
+        }
+        int level = 0;
+        int internalPathLength = 0;
+        Node current = root;
+        Node parent = null;
+        while (true) {
+            if (current.left != null || current.right != null) {
+
+            } else {
+                return internalPathLength;
             }
         }
     }
@@ -135,14 +286,29 @@ public class BST implements Tree {
         Just for testing
     */
     public static void main (String []args) {
-        BST newTree = new BST();
-        newTree.addNode(2);
-        newTree.addNode(1);
-        newTree.addNode(30);
-        newTree.addNode(10);
-        newTree.addNode(69);
-        newTree.addNode(74);
-        newTree.showTree();
+        BST tree = new BST();
+        tree.addNode(2);
+        tree.addNode(1);
+        tree.addNode(1);
+        tree.addNode(30);
+        tree.addNode(10);
+        tree.addNode(69);
+        tree.addNode(74);
+        tree.showTree();
+        System.out.println("Trying to find element 11. Successful: " + (tree.findNode(11) != null));
+        System.out.println("Trying to find element 1. Successful: " + (tree.findNode(1) != null));
+        System.out.println("Trying to find element 10. Successful: " + (tree.findNode(10) != null));
+        System.out.println("Length of the tree is: " + tree.getLength());
+        System.out.println("Emptiness of the tree is: " + tree.isEmpty());
+        System.out.println();
+        tree.removeNode(30);
+        tree.showTree();
+        System.out.println();
+        tree.clearTree();
+        tree.showTree();
+        System.out.println("Length of the tree is: " + tree.getLength());
+        System.out.println("Emptiness of the tree is: " + tree.isEmpty());
+        System.out.println("Trying to find element 10. Successful: " + (tree.findNode(10) != null));
     }
 
 }
